452a2854d87eacdef5f9fc848415b97f3b2df350
Jin <jinnkimm7@gmail.com>
2023-07-17 13:30:28 +0900

    Form을 만드는 밥법

--- basic-mentor/src/index.js
+++ basic-mentor/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppMentor from './AppMentor';
 import AppMentors from './AppMentors';
+import AppForm from './basic/Appform';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppForm />
   </React.StrictMode>
 );
