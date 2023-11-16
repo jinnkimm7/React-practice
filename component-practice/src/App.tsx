04983d9153b9dabe519ad7d43dc59928efc1025e
Jin <jinnkimm7@gmail.com>
2023-11-16 19:51:54 +0900

    Chore: 초기 설정

---
+++ component-practice/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-600'>Hello, Storybook!</h1>
+    </div>
+  );
+}
+
+export default App;
