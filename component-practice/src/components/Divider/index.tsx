3ad28983407ee2a660b57f348fc40fb7311a182d
Jin <jinnkimm7@gmail.com>
2023-11-27 16:17:16 +0900

    Feat: Divider 컴포넌트

--- component-practice/src/components/Divider/index.tsx
+++ component-practice/src/components/Divider/index.tsx
@@ -1,8 +1,21 @@
-export default function Divider() {
-  return (
-    <div>
-
-    </div>
-  );
+export interface DividerProps {
+  type: 'horizontal' | 'vertical';
+  marginSize?: number;
 }
 
+export default function Divider({
+  type = 'horizontal',
+  marginSize = 0,
+  ...props
+}: DividerProps) {
+
+  const horizontalClass = `block w-[100%] h-[1px] my-${marginSize}`;
+  const verticalClass = `relative top-[-1px] inline-block w-[1px] h-3 align-middle mx-${marginSize}`;
+
+  return (
+    <hr
+      className={`bg-gray-500 ${type === 'horizontal' ? horizontalClass : verticalClass}`}
+      {...props}
+    />
+  );
+}
---
+++ component-practice/src/stories/Divider.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Divider from '../components/Divider';
+import Text from '../components/Text';
+
+const meta: Meta<typeof Divider> = {
+  title: 'Components/Divider',
+  component: Divider,
+  argTypes: {
+    type: { control: 'inline-radio' },
+    marginSize: { control: { type: 'range', min: 0, max: 10 } }
+  },
+}
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Horizontal: Story = (args) => (
+  <>
+    <Text>Upper</Text>
+    <Divider {...args} />
+    <Text>Lower</Text>
+  </>
+);
+
+Horizontal.args = {
+  type: 'horizontal',
+}
+
+export const Vertical: Story = (args) => (
+  <>
+    <Text>Left</Text>
+    <Divider {...args} />
+    <Text>Right</Text>
+  </>
+)
+
+Vertical.args = {
+  type: 'vertical',
+}
